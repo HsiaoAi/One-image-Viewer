cd123e55850945b05896b54330a62454a2bcfb4b
HsiaoAi <ilct227@gmail.com>
2017-11-23 14:18:57 +0800

    Loading the UIImagePickerController

--- OneImageViewer/OneImageViewer/LandingViewController.swift
+++ OneImageViewer/OneImageViewer/LandingViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class LandingViewController: UIViewController {
+class LandingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // Properties
     @IBOutlet weak var pickUpButton: UIButton!
-    
+    weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpPickImageButton()
+        imagePicker.delegate = self
     }
-    
+
 }
 
 extension LandingViewController {
@@ -36,11 +39,29 @@ extension LandingViewController {
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
         pickUpButton.layer.addSublayer(gradientLayer)
-        
+
         // Shadow
         pickUpButton.layer.shadowColor = UIColor.CustomTheme.black25.cgColor
         pickUpButton.layer.shadowOpacity = 1.0
         pickUpButton.layer.shadowOffset = CGSize(width: 0, height: 2)
         pickUpButton.layer.shadowRadius = 4
+
+        // Target-Action
+        pickUpButton.addTarget(self, action: .loadImageButtonTapped, for: .touchUpInside)
     }
+
+    @objc func loadImageButtonTapped(_ sender: Any) {
+
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+// MARK: - Selector
+private extension Selector {
+
+    static let loadImageButtonTapped = #selector(LandingViewController.loadImageButtonTapped)
+
 }
