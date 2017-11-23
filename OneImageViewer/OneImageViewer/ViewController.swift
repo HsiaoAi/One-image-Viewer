0587f63b8dd489a2e75f32ad7a150f6cbf9d8c08
HsiaoAi <ilct227@gmail.com>
2017-11-23 13:12:58 +0800

    Add Assets

---
+++ OneImageViewer/OneImageViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneImageViewer
+//
+//  Created by Hsiao Ai LEE on 23/11/2017.
+//  Copyright © 2017 Hsiao Ai LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
